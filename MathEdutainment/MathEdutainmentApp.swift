ca0dc59d47da4325ae461a3baf8f096d6feba889
kazoo1122 <syayo181@gmail.com>
2022-10-22 14:15:33 +0900

    Initial Commit

---
+++ MathEdutainment/MathEdutainmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  MathEdutainmentApp.swift
+//  MathEdutainment
+//
+//  Created by Kazumasa Ohira on 2022/10/22.
+//
+
+import SwiftUI
+
+@main
+struct MathEdutainmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
